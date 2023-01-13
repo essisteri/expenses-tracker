5cfa23557d936e9bf18510b5b9b6170fa24582ae
essisteri <essi.ryynanen@tuni.fi>
2023-01-13 18:56:10 +0200

    cors

--- index.js
+++ index.js
@@ -3,14 +3,25 @@
 /* eslint-disable quotes */
 /* eslint-disable linebreak-style */
 
+/* app */
+
 const express = require("express");
+
+const cors = require("cors");
 const expensesRouter = require("./routes/expenses");
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5000", "https://expences-app-essi.onrender.com"],
+  })
+);
 
 app.use(express.json());
 
 app.use("/api/expenses", expensesRouter);
+
+/* index */
 
 const PORT = process.env.PORT || 3306;
 
